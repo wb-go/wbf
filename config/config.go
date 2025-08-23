9456966f4f452865ec7900ccd445dafc4c44b1f2
komil <komilovkomil156@gmail.com>
2025-08-23 22:32:55 +0300

    feat: added new methods to config package
    - GetInt - returns int config variable
    
    - Unmarshal - unmarshals config file to go structure
    
    - SetDefault - sets default value to config key
    
    all methods just call corresponding methods from viper library

--- config/config.go
+++ config/config.go
@@ -21,3 +21,15 @@ func (c *Config) Load(path string) error {
 func (c *Config) GetString(key string) string {
 	return c.v.GetString(key)
 }
+
+func (c *Config) GetInt(key string) int {
+	return c.v.GetInt(key)
+}
+
+func (c *Config) Unmarshal(rawVal any, opts ...viper.DecoderConfigOption) error {
+	return c.v.Unmarshal(rawVal, opts...)
+}
+
+func (c *Config) SetDefault(key string, value any) {
+	c.v.SetDefault(key, value)
+}
