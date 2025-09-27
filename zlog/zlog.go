1ec48d29d099f0cec1ce64cd41518ae402bdedba
golovanevvs <lony@inbox.ru>
2025-09-27 18:49:18 +0300

    zlog: feat: add InitConsole, SetLevel

--- zlog/zlog.go
+++ zlog/zlog.go
@@ -14,3 +14,31 @@ var Logger zerolog.Logger
 func Init() {
 	Logger = zerolog.New(os.Stdout).With().Timestamp().Logger()
 }
+
+// InitConsole инициализирует глобальный логгер с ConsoleWriter, цветной и удобный для чтения через Stdout.
+func InitConsole() {
+	Logger = zerolog.New(zerolog.ConsoleWriter{
+		Out:        os.Stdout,
+		TimeFormat: "2006-01-02 15:04:05",
+	}).With().
+		Timestamp().
+		Logger().
+		Level(zerolog.TraceLevel)
+}
+
+// SetLevel устанавливает уровень логирования для глобального логгера Logger.
+//
+// Параметры:
+//
+//	logLevelStr — строковое представление уровня логирования:
+//	"trace", "debug", "info", "warn", "error", "fatal", "panic".
+func SetLevel(logLevelStr string) error {
+	logLevel, err := zerolog.ParseLevel(logLevelStr)
+	if err != nil {
+		return err
+	}
+
+	Logger = Logger.Level(logLevel)
+
+	return nil
+}
